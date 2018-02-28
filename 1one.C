bb686d65ec418799c40af3064f3114f8ece2dea8
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-28 03:26:54 -0800

    1one.C

---
+++ 1one.C
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number :");
+scanf("%d",&num);
+switch(num)
+{
+ case 1:
+ printf("\n ONE");
+ break;
+ case 2:
+ printf("\n TWO");
+ break;
+ case 3:
+ printf("\n THREE");
+ break;
+ case 4:
+ printf("\n FOUR");
+ break;
+ case 5:
+ printf("\n FIVE");
+ break;
+ case 6:
+ printf("\n SIX");
+ break;
+ case 7:
+ printf("\n SEVEN");
+ break;
+ case 8:
+ printf("\n EIGHT");
+ break;
+ case 9:
+ printf("\n NINE");
+ break;
+ case 10:
+ printf("\n TEN");
+ break;
+ default:
+ printf("invalid");
+ }
+ return 0;
+ }
+ 
